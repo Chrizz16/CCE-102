d15f028338c39974df979cbdfbff1d04662a6548
Chrizz16 <c.abarento.569484@umindanao.edu.ph>
2025-12-04 15:35:11 +0800

    Add files via upload

--- The_Smart_Home_Energy_Auditor.java
+++ The_Smart_Home_Energy_Auditor.java
@@ -1,71 +1,48 @@
 package mypackage;
-import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
-
 public class The_Smart_Home_Energy_Auditor {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		File file = new File("energy_audit.txt");
-		try {
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-		} catch (Exception e) {
-			System.out.println("Something went wrong!");
-		}
-		
-		int[] input = scanDevices(sc);
-		String getWatts = checkEfficiency(input);
-		saveAudit(getWatts, file);
-		
-		sc.close();
-		    
-	}
-	
-	public static int[] scanDevices(Scanner scan) {
 		System.out.print("How many devices to scan? ");
-		int numDev = scan.nextInt();
-		scan.nextLine();
-		int[] wattage = new int[numDev];
-		System.out.printf("Enter wattage for %d devices:%n", numDev);
+		int devices = sc.nextInt();	
+		String data =  "";
+		int[] dev = scanDevices(sc, devices);
+		for (int i = 0; i < dev.length; i++) {
+			data += "Device " + (i+1) + ": " + dev[i] + "W - " + checkEfficiency(dev[i]) + "\n";
+		}
+		System.out.println("\n"+data);
+		saveAudit(data);
+		System.out.println("Energy audit saved to energy_audit.txt");
+		sc.close();
+	}
+
+	public static int[] scanDevices(Scanner sc, int devices) {
+		System.out.println("Enter wattage for " + devices + " devices:");
+		int[] wattage = new int[devices];
 		for (int i = 0; i < wattage.length; i++) {
-			System.out.printf("Device %d: ", (i+1));
-			wattage[i]= scan.nextInt();
-			scan.nextLine();
+			System.out.print("Device " + (i + 1) + ": ");
+			wattage[i] = sc.nextInt();
+			sc.nextLine();
 		}
 		return wattage;
-		
-		
 	}
-	
-	public static String checkEfficiency(int[] watts) {
-		String contain = "";
-		for (int i = 0; i < watts.length; i++) {
-			if (watts[i] > 1500) {
-				contain += "Device " + (i+1) + ": " + watts[i] + "W - " + "Power Hog\n";
-			} else if(watts[i] >= 200) {
-				contain += "Device " + (i+1) + ": " + watts[i] + "W - " + "Standard\n";
-			} else {
-				contain += "Device " + (i+1) + ": " + watts[i] + "W - " + "Eco-Friendly\n";
-			}
-			
+	public static String checkEfficiency(int watts) {
+		if (watts < 200 ) {
+			return "Eco-Friendly";
+		} else if (watts <= 1500) {
+			return "Standard";
+		} else {
+			return "Power Hog";
 		}
-		System.out.println();
-		System.out.println(contain);
-		return contain;
 	}
-	
-	public static void saveAudit(String text, File file) {
-		try {
-			FileWriter writer = new FileWriter(file, true);
-			writer.write(text);
-			writer.close();
-			System.out.println("Energy audit saved to energy_audit.txt");
+	public static void saveAudit (String data) {
+		 try {
+			FileWriter fw = new FileWriter("energy_audit.txt", true);
+			fw.write(data);
+			fw.close();
 		} catch (Exception e) {
-			System.out.println("Something went wrong!");
+			System.out.println("Error: " + e);
 		}
 	}
-	
 }
